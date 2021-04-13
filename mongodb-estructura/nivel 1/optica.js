68b710160966ebcf0d0b8c06d9d1f329d2740cb5
Pilar Mingueza <74919850+pmh-itacademy@users.noreply.github.com>
2021-04-13 09:42:52 +0200

    Eliminar acentos

--- mongodb-estructura/nivel 1/optica.js
+++ mongodb-estructura/nivel 1/optica.js
@@ -3,13 +3,13 @@ db.dropDatabase();
 db.createCollection('proveidor');
 db.proveidor.insertOne({
 	nom: 'Ochi',
-	adreça: { carrer: 'c/Sole', num: '1', pis: null, porta: null, ciutat: "Palermo", pais: "Italia", cod_postal: "90121" },
+	adresa: { carrer: 'c/Sole', num: '1', pis: null, porta: null, ciutat: "Palermo", pais: "Italia", cod_postal: "90121" },
 	marques: [{
 		marca_id: 1,
 		marca_nom: "ochi preziosi",
 		ulleres: [
-			{ ullera_id: 10, graduació: [1.25, 0.50], tipus_muntura: "pasta", color_muntura: "blau", color_vidres: [null, null], preu: 200.15 },
-			{ ullera_id: 20, graduació: [0, 0], tipus_muntura: "flotant", color_muntura: "plata", color_vidres: ["blau", "blau"], preu: 50.50 },
+			{ ullera_id: 10, graduacio: [1.25, 0.50], tipus_muntura: "pasta", color_muntura: "blau", color_vidres: [null, null], preu: 200.15 },
+			{ ullera_id: 20, graduacio: [0, 0], tipus_muntura: "flotant", color_muntura: "plata", color_vidres: ["blau", "blau"], preu: 50.50 },
 
 		]
 	},
@@ -17,8 +17,8 @@ db.proveidor.insertOne({
 		marca_id: 2,
 		marca_nom: "marca nuova",
 		ulleres: [
-			{ ullera_id: 30, graduació: [-3, -2], tipus_muntura: "flotant", color_muntura: "blanc", color_vidres: [null, null], preu: 333.22 },
-			{ ullera_id: 40, graduació: [0, 0], tipus_muntura: "flotant", color_muntura: "plata", color_vidres: ["verd", "verd fosc"], preu: 114 },
+			{ ullera_id: 30, graduacio: [-3, -2], tipus_muntura: "flotant", color_muntura: "blanc", color_vidres: [null, null], preu: 333.22 },
+			{ ullera_id: 40, graduacio: [0, 0], tipus_muntura: "flotant", color_muntura: "plata", color_vidres: ["verd", "verd fosc"], preu: 114 },
 
 		]
 	}],
@@ -26,23 +26,23 @@ db.proveidor.insertOne({
 db.proveidor.insertOne({
 
 	nom: "Glasses",
-	adreça: {
+	adresa: {
 		carrer: "High Street", num: "34", pis: "Ground fl", porta: "A", ciutat: "Esher", pais: "UK", cod_postal: "KT9APL5"
 	},
 	marques: [{
 		marca_id: 3,
 		marca_nom: "Rey Bon",
 		ulleres: [
-			{ ullera_id: 50, graduació: [2.5, 3], tipus_muntura: "metal.lica", color_muntura: "rosa", color_vidres: [null, null], preu: 444.14 },
-			{ ullera_id: 60, graduació: [0, 0], tipus_muntura: "flotant", color_muntura: "gris", color_vidres: ["gris", "gris"], preu: 212.5 }
+			{ ullera_id: 50, graduacio: [2.5, 3], tipus_muntura: "metal.lica", color_muntura: "rosa", color_vidres: [null, null], preu: 444.14 },
+			{ ullera_id: 60, graduacio: [0, 0], tipus_muntura: "flotant", color_muntura: "gris", color_vidres: ["gris", "gris"], preu: 212.5 }
 		]
 	},
 	{
 		marca_id: 4,
 		marca_nom: "Vague",
 		ulleres: [
-			{ ullera_id: 70, graduació: [1, 1], tipus_muntura: "pasta", color_muntura: "negra", color_vidres: [null, null], preu: 111.11 },
-			{ ullera_id: 80, graduació: [2, 2], tipus_muntura: "pasta", color_muntura: "blau", color_vidres: ["blau", "blau fosc"], preu: 232.43 }
+			{ ullera_id: 70, graduacio: [1, 1], tipus_muntura: "pasta", color_muntura: "negra", color_vidres: [null, null], preu: 111.11 },
+			{ ullera_id: 80, graduacio: [2, 2], tipus_muntura: "pasta", color_muntura: "blau", color_vidres: ["blau", "blau fosc"], preu: 232.43 }
 		]
 	}]
 });
@@ -54,7 +54,7 @@ db.client.insertOne({
 	telefon: "123123123",
 	email: "pepepe@gmail.com",
 	data_registre: new Date("2020-12-11T14:12:00Z"),
-	adreça: { carrer: 'c/Sol', num: '19', pis: 3, porta: "B", provincia_id: 8, localitat_id: 37, cod_postal: "08870" },
+	adresa: { carrer: 'c/Sol', num: '19', pis: 3, porta: "B", provincia_id: 8, localitat_id: 37, cod_postal: "08870" },
 	ulleres: [{ullera_id: 70,empleat: "Juan García"}]
 });
 db.client.insertOne({
@@ -64,7 +64,7 @@ db.client.insertOne({
 	telefon: "931234567",
 	email: "lola@gmail.com",
 	data_registre: new Date("2021-03-11T18:12:00Z"),
-	adreça: { carrer: 'c/Sol', num: '19', pis: 3, porta: "B", provincia_id: 8, localitat_id: 37, cod_postal: "08870" },
+	adresa: { carrer: 'c/Sol', num: '19', pis: 3, porta: "B", provincia_id: 8, localitat_id: 37, cod_postal: "08870" },
 	ulleres: [{ullera_id: 40,empleat: "Juan García"}]
 });
 db.createCollection('provincia');
--- mongodb-estructura/nivel 3/spotify.js
+++ mongodb-estructura/nivel 3/spotify.js
@@ -12,8 +12,8 @@ db.usuari.insertOne({
 	usuari_pais: "Spain",
 	usuari_codpostal: "08870",
 	suscripcio: [
-		{ data_inici: new Date("2000-12-11"), data_renovació: new Date("2001-12-11"), forma_pagament: "P" },
-		{ data_inici: new Date("2020-12-11"), data_renovació: new Date("2021-12-11"), forma_pagament: "C" }],
+		{ data_inici: new Date("2000-12-11"), data_renovacio: new Date("2001-12-11"), forma_pagament: "P" },
+		{ data_inici: new Date("2020-12-11"), data_renovacio: new Date("2021-12-11"), forma_pagament: "C" }],
 	targeta: { targeta_num: "1234234534564567", targeta_mes: "03", targeta_any: "2025", targeta_codi: "023" },
 	pagament: [
 		{ pagament_id: 1, pagament_data: new Date("2000-12-11"), pagament_total: 33.45 },
@@ -21,7 +21,7 @@ db.usuari.insertOne({
 		{ pagament_id: 3, pagament_data: new Date("2020-12-11"), pagament_total: 40.33 },
 	],
 	segueix: [2, 3, 5],
-	canço_favorita: [1],
+	cancion_favorita: [1],
 	album_favorit: [1, 2]
 });
 db.usuari.insertOne({
@@ -35,7 +35,7 @@ db.usuari.insertOne({
 	usuari_pais: "Spain",
 	usuari_codpostal: "34570",
 	segueix: [1, 2],
-	canço_favorita: [2, 3],
+	cancion_favorita: [2, 3],
 	album_favorit: [3]
 });
 db.usuari.insertOne({
@@ -49,15 +49,15 @@ db.usuari.insertOne({
 	usuari_pais: "UK",
 	usuari_codpostal: "KT10UPL",
 	suscripcio: [
-		{ data_inici: new Date("2020-01-01"), data_renovació: new Date("2021-01-01"), forma_pagament: "P" },
-		{ data_inici: new Date("2021-02-01"), data_renovació: new Date("2022-02-01"), forma_pagament: "C" }],
+		{ data_inici: new Date("2020-01-01"), data_renovacio: new Date("2021-01-01"), forma_pagament: "P" },
+		{ data_inici: new Date("2021-02-01"), data_renovacio: new Date("2022-02-01"), forma_pagament: "C" }],
 	paypal: "lolag1@gmail.com",
 	pagament: [
 		{ pagament_id: 1, pagament_data: new Date("2020-01-01"), pagament_total: 30.00 },
 		{ pagament_id: 2, pagament_data: new Date("2001-02-01"), pagament_total: 35.99 }
 	],
 	segueix: [1, 4],
-	canço_favorita: [3, 4],
+	cancion_favorita: [3, 4],
 	album_favorit: [2]
 });
 db.createCollection('playlist');
@@ -69,20 +69,20 @@ db.playlist.insertOne({
 	playlist_usuari_crea: 2,
 	playlist_estat: "M",
 	playlist_data_borrado: new Date("2021-03-20"),
-	playlist_canço: [
+	playlist_cancion: [
 		{
-			cançó_id: 1,
-			cançó_títol: "Mediterraneo",
-			cançó_afegida_per: 2,
-			cançó_afegida_data: new Date("2021-03-20"),
-			cançó_artista: "Joan Manel Serrat"
+			cancion_id: 1,
+			cancion_titulo: "Mediterraneo",
+			cancion_afegida_per: 2,
+			cancion_afegida_data: new Date("2021-03-20"),
+			cancion_artista: "Joan Manel Serrat"
 		},
 		{
-			cançó_id: 2,
-			cançó_títol: "Yesterday",
-			cançó_afegida_per: 2,
-			cançó_afegida_data: new Date("2021-03-21"),
-			cançó_artista: "The Beatles"
+			cancion_id: 2,
+			cancion_titulo: "Yesterday",
+			cancion_afegida_per: 2,
+			cancion_afegida_data: new Date("2021-03-21"),
+			cancion_artista: "The Beatles"
 		},
 	]
 });
@@ -92,17 +92,17 @@ db.artista.insertOne({
 	artista_nom: "Joan Manuel Serrat",
 	album: [
 		{
-			album_id: 1, album_titol: "Mediterraneo", album_data_publicació: new Date("1971-03-21"),
-			canço: [
-				{ cançó_id: 1, cançó_títol: "Mediterraneo", cançó_durada: 230, cançó_reproduccions: 234 },
-				{ cançó_id: 2, cançó_títol: "Aquellas pequeñas cosas", cançó_durada: 160, cançó_reproduccions: 934 },
+			album_id: 1, album_titol: "Mediterraneo", album_data_publicacio: new Date("1971-03-21"),
+			cancion: [
+				{ cancion_id: 1, cancion_titulo: "Mediterraneo", cancion_durada: 230, cancion_reproduccions: 234 },
+				{ cancion_id: 2, cancion_titulo: "Aquellas pequeñas cosas", cancion_durada: 160, cancion_reproduccions: 934 },
 			]
 		},
 		{
-			album_id: 2, album_titol: "Miguel Hernández", album_data_publicació: new Date("1972-03-21"),
-			canço: [
-				{ cançó_id: 3, cançó_títol: "Elegía", cançó_durada: 210, cançó_reproduccions: 456 },
-				{ cançó_id: 4, cançó_títol: "Para la libertad", cançó_durada: 190, cançó_reproduccions: 123 }
+			album_id: 2, album_titol: "Miguel Hernández", album_data_publicacio: new Date("1972-03-21"),
+			cancion: [
+				{ cancion_id: 3, cancion_titulo: "Elegía", cancion_durada: 210, cancion_reproduccions: 456 },
+				{ cancion_id: 4, cancion_titulo: "Para la libertad", cancion_durada: 190, cancion_reproduccions: 123 }
 			]
 		}
 	],
@@ -113,17 +113,17 @@ db.artista.insertOne({
 	artista_nom: "Joaquín Sabina",
 	album: [
 		{
-			album_id: 3, album_titol: "Física y química", album_data_publicació: new Date("1992-01-01"),
-			canço: [
-				{ cançó_id: 5, cançó_títol: "Y nos dieron las diez", cançó_durada: 230, cançó_reproduccions: 1222 },
-				{ cançó_id: 6, cançó_títol: "Conductores suicidas", cançó_durada: 160, cançó_reproduccions: 565 },
+			album_id: 3, album_titol: "Física y química", album_data_publicacio: new Date("1992-01-01"),
+			cancion: [
+				{ cancion_id: 5, cancion_titulo: "Y nos dieron las diez", cancion_durada: 230, cancion_reproduccions: 1222 },
+				{ cancion_id: 6, cancion_titulo: "Conductores suicidas", cancion_durada: 160, cancion_reproduccions: 565 },
 			]
 		},
 		{
-			album_id: 4, album_titol: "Esta boca es mia", album_data_publicació: new Date("1994-01-21"),
-			canço: [
-				{ cançó_id: 7, cançó_títol: "Esta noche contigo", cançó_durada: 210, cançó_reproduccions: 456 },
-				{ cançó_id: 8, cançó_títol: "Ruido", cançó_durada: 190, cançó_reproduccions: 123 }
+			album_id: 4, album_titol: "Esta boca es mia", album_data_publicacio: new Date("1994-01-21"),
+			cancion: [
+				{ cancion_id: 7, cancion_titulo: "Esta noche contigo", cancion_durada: 210, cancion_reproduccions: 456 },
+				{ cancion_id: 8, cancion_titulo: "Ruido", cancion_durada: 190, cancion_reproduccions: 123 }
 			]
 		}
 	],
